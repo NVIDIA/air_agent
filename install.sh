29fc3d6963d1934cb2a70c4a37821e18d8d8a178
Michael Robertson <mrobertson@cumulusnetworks.com>
2020-03-16 16:32:47 -0400

    Add VRF support

--- install.sh
+++ install.sh
@@ -6,6 +6,30 @@ if [ $who != 'root' ]; then
     exit 1
 fi
 
+while getopts ":v:" opt; do
+    case "${opt}" in
+        v)
+            AIR_VRF=${OPTARG}
+            ;;
+    esac
+done
+
+install_service() {
+    if [ -z "$1" ]; then
+        cp ./air-agent.service /etc/systemd/system/air-agent.service
+        systemctl enable air-agent
+        systemctl start air-agent
+    else
+        echo "Enabling in VRF ${1}..."
+        IP_BIN=`which ip`
+        cp ./air-agent@.service /etc/systemd/system/air-agent@.service
+        sed -i 's,{1},'"${IP_BIN}"',' /etc/systemd/system/air-agent@.service
+        sed -i "s/{2}/${1}/" /etc/systemd/system/air-agent@.service
+        systemctl enable air-agent
+        systemctl start air-agent
+    fi
+}
+
 echo "####################################"
 echo "# Installing pip requirements      #"
 echo "####################################"
@@ -41,9 +65,7 @@ echo "Done!"
 echo "####################################"
 echo "# Enabling systemd service         #"
 echo "####################################"
-cp ./air-agent.service /etc/systemd/system/air-agent.service
-systemctl enable air-agent
-systemctl start air-agent
+install_service $AIR_VRF
 echo "Done!"
 
 exit 0
