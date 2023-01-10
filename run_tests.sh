79c1acf9520f9954160cd4f5549b0588fc4a0a73
Nick Mitchell <nmitchell@nvidia.com>
2023-01-10 14:09:32 -0800

    Update license headers for 2023

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 coverage run --omit='tests/*,**/__init__.py,version.py' --source='./' `which pytest` tests/tests.py
